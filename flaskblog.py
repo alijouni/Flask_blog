3e90a596e4d38cabc40fe0e02f40a173da09f198
Ali Jouni <alijouni@outlook.de>
2021-12-10 23:06:10 +0200

    Forms validation

--- flaskblog.py
+++ flaskblog.py
@@ -4,6 +4,7 @@ from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
+#dummy JSON data 
 posts = [
     {
         'author': 'Corey Schafer',
